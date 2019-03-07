a6edf4bc8127bc14adcc8763a50957998a76e4ce
lius <lius@kayak.com.cn>
2019-03-07 20:44:16 +0800

    初始化

---
+++ rs-payroll/src/main/java/chain/fxgj/server/payroll/dto/ent/EntInfoDTO.java
@@ -0,0 +1,116 @@
+package chain.fxgj.server.payroll.dto.ent;
+
+import lombok.*;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class EntInfoDTO {
+
+    /**
+     * 企业id
+     */
+    private String entId;
+    /**
+     * 企业名称
+     */
+    private String entName;
+    /**
+     * 企业简称
+     */
+    private String shortEntName;
+    /**
+     * 员工在职状态是否[在职]（ true[在职]   false ）
+     */
+    private Boolean empEntInservice;
+    /**
+     * 机构信息
+     */
+    @Builder.Default
+    private HashMap<String, GroupInfo> groupInfoMap = new HashMap<String, GroupInfo>();
+    /**
+     * 机构信息
+     */
+    @Builder.Default
+    private LinkedList<GroupInfo> groupInfoList = new LinkedList<GroupInfo>();
+
+    @Getter
+    @Setter
+    @EqualsAndHashCode
+    @ToString
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class GroupInfo {
+        /**
+         * 机构id
+         */
+        private String groupId;
+        /**
+         * 机构名称
+         */
+        private String groupName;
+        /**
+         * 机构机构
+         */
+        private String groupShortName;
+        /**
+         * 员工在职状态是否[在职]（ true[在职]   false ）
+         */
+        private Boolean empGroupInservice;
+        /**
+         * 员工信息
+         */
+        EmployeeInfo employeeInfo;
+
+
+        @Getter
+        @Setter
+        @EqualsAndHashCode
+        @ToString
+        @Builder
+        @NoArgsConstructor
+        @AllArgsConstructor
+        public static class EmployeeInfo {
+            /**
+             * 员工id
+             */
+            private String employeeId;
+            /**
+             * 姓名
+             */
+            private String employeeName;
+            /**
+             * 在职状态
+             */
+            private Integer employeeStatus;
+            /**
+             * 在职描述
+             */
+            private String employeeStatusDesc;
+            /**
+             * 手机号
+             */
+            private String phone;
+            /**
+             * 入职时间
+             */
+            private Long entryDate;
+            /**
+             * 职位
+             */
+            private String position;
+            /**
+             * 员工工号
+             */
+            private String employeeNo;
+        }
+    }
+}
--- rs-payroll/src/main/java/chain/fxgj/server/payroll/web/UserPrincipal.java
+++ rs-payroll/src/main/java/chain/fxgj/server/payroll/web/UserPrincipal.java
@@ -1,112 +1,132 @@
 package chain.fxgj.server.payroll.web;
 
+import chain.fxgj.server.payroll.dto.ent.EntInfoDTO;
+import lombok.*;
+
 import java.security.Principal;
-import java.time.Duration;
 import java.time.LocalDateTime;
-import java.util.Arrays;
+import java.util.List;
 
 /**
  * @author chain
  * create by chain on 2018/8/20 下午10:30
  **/
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString(exclude = {"entInfoDTOS"})
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserPrincipal implements Principal {
-    String sessionId;
-    String name;
-    String[] roles;
-    Duration sessionTimeOut;
+    /**
+     * sessionId
+     */
+    private String sessionId;
+    /**
+     * 用户名
+     */
+    private String name;
+    /**
+     * 角色列表
+     */
+    private String[] roles;
+    /**
+     * 用户session超时时间
+     */
+    private LocalDateTime sessionTimeOut;
+    /**
+     * 用户名
+     */
+    private String userName;
+    /**
+     * 企业id
+     */
+    private String entId;
+    /**
+     * 企业名称
+     */
+    private String entName;
+    /**
+     * 用户 微信openId
+     */
+    private String openId;
+    /**
+     * 用户身份证号码（明文）
+     */
+    private String idNumber;
+    /**
+     * 用户身份证号码（密文）
+     */
+    private String idNumberEncrytor;
+    /**
+     * 用户 手机号
+     */
+    private String phone;
+    /**
+     * 用户昵称
+     */
+    private String nickname;
+    /**
+     * 用户头像url
+     */
+    private String headimgurl;
+    /**
+     * 用户绑定微信表 唯一标识
+     */
+    private String wechatId;
+    /**
+     * 用户查询密码
+     */
+    private String queryPwd;
+    /**
+     * 放薪经理标识
+     */
+    private String managerId;
+    /**
+     * 分行号
+     */
+    private String branchNo;
+    /**
+     * 用户 岗位
+     */
+    private String officer;
+    /**
+     * 分行号
+     */
+    private String subBranchNo;
+    /**
+     * 用户所在机构列表
+     */
+    private String[] groupIds;
+    /**
+     * 用户 账户列
+     */
+    private String[] accouts;
+    /**
+     * 用户登陆时间
+     */
+    @Builder.Default
+    private LocalDateTime loginDateTime = LocalDateTime.now();
+    /**
+     * 用户超时时间
+     */
+    private Integer timeOutMinute;
+    /**
+     * 用户最后操作时间
+     */
+    private LocalDateTime lastOptDateTime;
+    /**
+     * 用户所在企业列表
+     */
+    private List<EntInfoDTO> entInfoDTOS;
 
-    String userName;
 
-    LocalDateTime loginDateTime;
-
-    LocalDateTime lastOptDateTime;
-    boolean isAdmin;
-
-    String orgId;
-
-    public UserPrincipal() {
-        this.loginDateTime = LocalDateTime.now();
-    }
-
-    public UserPrincipal(String useId, String userName, String sessionId, Duration sessionTimeOut, String[] roles, boolean isAdmin) {
-        this.name = useId;
-        this.userName = userName;
+    public UserPrincipal(String name, String sessionId, LocalDateTime sessionTimeOut, String[] roles) {
+        this.name = name;
         this.sessionId = sessionId;
         this.sessionTimeOut = sessionTimeOut;
         this.roles = roles;
         this.loginDateTime = LocalDateTime.now();
-        this.lastOptDateTime = LocalDateTime.now();
-        this.isAdmin = isAdmin;
-    }
-
-    public Duration getSessionTimeOut() {
-        return sessionTimeOut;
-    }
-
-    public String getSessionId() {
-        return sessionId;
-    }
-
-    public boolean isAdmin() {
-        return isAdmin;
-    }
-
-    public void setAdmin(boolean admin) {
-        isAdmin = admin;
-    }
-
-    public String[] getRoles() {
-        return roles;
-    }
-
-
-    @Override
-    public String getName() {
-        return name;
-    }
-
-
-    public String getUserName() {
-        return userName;
-    }
-
-
-    @Override
-    public String toString() {
-        return "UserPrincipal{" +
-                "sessionId='" + sessionId + '\'' +
-                ", name='" + name + '\'' +
-                ", roles=" + Arrays.toString(roles) +
-                ", sessionTimeOut=" + sessionTimeOut +
-                ", userName='" + userName + '\'' +
-                ", loginDateTime=" + loginDateTime +
-                ", lastOptDateTime=" + lastOptDateTime +
-                ", isAdmin=" + isAdmin +
-                ", orgId='" + orgId + '\'' +
-                '}';
-    }
-
-    public LocalDateTime getLoginDateTime() {
-        return loginDateTime;
-    }
-
-    public void setLoginDateTime(LocalDateTime loginDateTime) {
-        this.loginDateTime = loginDateTime;
-    }
-
-    public LocalDateTime getLastOptDateTime() {
-        return lastOptDateTime;
-    }
-
-    public void setLastOptDateTime(LocalDateTime lastOptDateTime) {
-        this.lastOptDateTime = lastOptDateTime;
-    }
-
-    public String getOrgId() {
-        return orgId;
-    }
-
-    public void setOrgId(String orgId) {
-        this.orgId = orgId;
     }
 }
