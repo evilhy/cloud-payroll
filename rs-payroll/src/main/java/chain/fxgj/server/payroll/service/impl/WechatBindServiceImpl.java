f8bee4a7ab042b4247e68ad0b60813e414aadde5
lius <lius@kayak.com.cn>
2019-03-12 15:14:08 +0800

    去掉

--- rs-payroll/src/main/java/chain/fxgj/server/payroll/service/impl/WechatBindServiceImpl.java
+++ rs-payroll/src/main/java/chain/fxgj/server/payroll/service/impl/WechatBindServiceImpl.java
@@ -22,8 +22,6 @@ import com.querydsl.core.types.ExpressionUtils;
 import com.querydsl.core.types.OrderSpecifier;
 import com.querydsl.core.types.Predicate;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
