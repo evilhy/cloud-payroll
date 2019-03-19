e635f77050d6681d488fe081e0d31c335d51d76e
kayak_zhucj <zhuchangjian@kayak.com.cn>
2019-03-19 15:13:15 +0800

    改中横线

--- rs-payroll/src/main/java/chain/fxgj/server/payroll/constant/PayrollConstants.java
+++ rs-payroll/src/main/java/chain/fxgj/server/payroll/constant/PayrollConstants.java
@@ -5,15 +5,15 @@ package chain.fxgj.server.payroll.constant;
  * create by chain on 2018/10/31 4:18 PM
  **/
 public class PayrollConstants {
-    public final static String JSESSIONID = "jsession_id";
+    public final static String JSESSIONID = "jsession-id";
 
     public final static String PREFIX = "fxgj";
-    public final static String LOG_TOKEN = "log_token";  //日志
-    public final static String JSESSION_ID = "jsession_id";    //登录会话ID
-    public final static String LOGIN_USER_ID = "login_user_id";    //登录用户ID
-    public final static String LOGIN_CN_NAME = "login_cn_name";    //登录用户姓名
-    public final static String LOGIN_ORG_ID = "login_org_id";    //登录用户所属机构
-    public final static String PAGE_NUM = "page_num";    //分页页码起始为1
+    public final static String LOG_TOKEN = "log-token";  //日志
+    public final static String JSESSION_ID = "jsession-id";    //登录会话ID
+    public final static String LOGIN_USER_ID = "login-user-id";    //登录用户ID
+    public final static String LOGIN_CN_NAME = "login-cn-name";    //登录用户姓名
+    public final static String LOGIN_ORG_ID = "login-org-id";    //登录用户所属机构
+    public final static String PAGE_NUM = "page-num";    //分页页码起始为1
     public final static String LIMIT = "limit";    //每页显示条数，默认为20
     public final static String SORT_FIELD = "sort_field";    //排序字段名
     public final static String DIRECTION = "direction";    //排序方向：DESC-倒序，ASC-正序。默认为ASC
