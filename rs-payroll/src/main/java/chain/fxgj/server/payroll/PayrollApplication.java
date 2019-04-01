97fdd9ec3c60157bbc14eea845800b96ad4f3519
kayak_zhucj <zhuchangjian@kayak.com.cn>
2019-04-01 12:10:55 +0800

    注释掉consul-1

--- rs-payroll/src/main/java/chain/fxgj/server/payroll/PayrollApplication.java
+++ rs-payroll/src/main/java/chain/fxgj/server/payroll/PayrollApplication.java
@@ -3,8 +3,6 @@ package chain.fxgj.server.payroll;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-//import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
