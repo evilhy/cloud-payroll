86b7f016d398c769945203b2f96b195b202d89bf
18627965660 <zaipengdu@163.com>
2019-06-12 18:52:14 +0800

    轮播图广告功能修改

--- rs-payroll/src/main/java/chain/fxgj/core/common/service/impl/AdvertisementServiceImpl.java
+++ rs-payroll/src/main/java/chain/fxgj/core/common/service/impl/AdvertisementServiceImpl.java
@@ -66,13 +66,13 @@ public class AdvertisementServiceImpl implements AdvertisementService {
     @Override
     public List<AdvertisingRotationDTO> rotation(int channelId, String entId) {
         /** 根据企业ID获取渠道银行、版本（目前仅获取 华夏银行——普通版——普通版） **/
-        EntErpriseInfo entErpriseInfo = entErpriseInfoDao.findById(entId).get();
-        FundLiquidationEnum fundLiquidationEnum = entErpriseInfo.getLiquidation();
-        VersionsTypeEnum versionsTypeEnum = entErpriseInfo.getVersion();
-        VersionsEnum versionsEnum = entErpriseInfo.getSubVersion();
-//        FundLiquidationEnum fundLiquidationEnum = FundLiquidationEnum.HXB;
-//        VersionsTypeEnum versionsTypeEnum = VersionsTypeEnum.NORMAL;
-//        VersionsEnum versionsEnum = VersionsEnum.NORMAL;
+//        EntErpriseInfo entErpriseInfo = entErpriseInfoDao.findById(entId).get();
+//        FundLiquidationEnum fundLiquidationEnum = entErpriseInfo.getLiquidation();
+//        VersionsTypeEnum versionsTypeEnum = entErpriseInfo.getVersion();
+//        VersionsEnum versionsEnum = entErpriseInfo.getSubVersion();
+        FundLiquidationEnum fundLiquidationEnum = FundLiquidationEnum.HXB;
+        VersionsTypeEnum versionsTypeEnum = VersionsTypeEnum.NORMAL;
+        VersionsEnum versionsEnum = VersionsEnum.NORMAL;
 
         QLiquidationVersionAdvertisingInfo qLiquidationVersionAdvertisingInfo = QLiquidationVersionAdvertisingInfo.liquidationVersionAdvertisingInfo;
         QAdvertisingInfo qAdvertisingInfo = QAdvertisingInfo.advertisingInfo;
