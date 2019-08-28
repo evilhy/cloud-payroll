d8ba6d4eeb5f99090f1b30bb8a88a88dcf0889e9
kayak_zhucj <zhuchangjian@kayak.com.cn>
2019-08-28 22:19:41 +0800

    增加版本4

--- rs-payroll/src/main/java/chain/fxgj/server/payroll/controller/AdvertisingRS.java
+++ rs-payroll/src/main/java/chain/fxgj/server/payroll/controller/AdvertisingRS.java
@@ -32,8 +32,8 @@ public class AdvertisingRS {
 
     @Autowired
     AdvertisementService advertisementService;
-    @Autowired
-    AdvertisingFeignService advertisingFeignService;
+//    @Autowired
+//    AdvertisingFeignService advertisingFeignService;
 
     /**
      * 轮播图查询
@@ -55,7 +55,7 @@ public class AdvertisingRS {
 //            List<AdvertisingRotationDTO> advertisingRotationDTOS = advertisementService.rotation(channelId, liquidation);
 //            return advertisingRotationDTOS;
 //            Mono<List<AdvertisingRotationDTO>> rotation = advertisingFeignController.rotation(2);
-            List<AdvertisingRotationDTO> advertisingRotationDTOS = advertisingFeignService.rotationList(channelId, liquidation);
+            List<AdvertisingRotationDTO> advertisingRotationDTOS = null;//advertisingFeignService.rotationList(channelId, liquidation);
             if (null != advertisingRotationDTOS) {
                 String url = advertisingRotationDTOS.get(0).getUrl();
                 log.info("success url:[{}]",url);
