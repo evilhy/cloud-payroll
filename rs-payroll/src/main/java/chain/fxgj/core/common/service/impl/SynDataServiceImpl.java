d41512d1d095832d4684a044b136930e14c9f04e
jinlin <305686443@qq.com>
2019-08-30 14:04:18 +0800

    完善数据同步方法

--- rs-payroll/src/main/java/chain/fxgj/core/common/service/impl/SynDataServiceImpl.java
+++ rs-payroll/src/main/java/chain/fxgj/core/common/service/impl/SynDataServiceImpl.java
@@ -2,14 +2,10 @@ package chain.fxgj.core.common.service.impl;
 
 import chain.fxgj.core.common.dto.employee.EmployeeCardDTO;
 import chain.fxgj.core.common.service.SynDataService;
-import chain.fxgj.core.jpa.dao.EmployeeCardInfoDao;
-import chain.fxgj.core.jpa.dao.EmployeeInfoDao;
-import chain.fxgj.core.jpa.dao.WageDetailInfoDao;
+import chain.fxgj.core.jpa.dao.*;
 import chain.fxgj.core.jpa.model.*;
 import chain.payroll.client.feign.SynDataFeignController;
-import chain.payroll.dto.sync.EmployeeCardInfoDTO;
-import chain.payroll.dto.sync.EmployeeInfoDTO;
-import chain.payroll.dto.sync.WageDetailInfoDTO;
+import chain.payroll.dto.sync.*;
 import com.google.gson.JsonObject;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
@@ -31,6 +27,17 @@ public class SynDataServiceImpl implements SynDataService {
 
     @Autowired
     private EmployeeInfoDao employeeInfoDao;
+    @Autowired
+    private EmployeeWechatInfoDao employeeWechatInfoDao;
+
+    @Autowired
+    private EntErpriseInfoDao entErpriseInfoDao;
+
+    @Autowired
+    private EntGroupInfoDao entGroupInfoDao;
+
+    @Autowired
+    private ManagerInfoDao managerInfoDao;
 
     @Autowired
     private EmployeeCardInfoDao employeeCardInfoDao;
@@ -47,10 +54,8 @@ public class SynDataServiceImpl implements SynDataService {
     @Override
     public Integer wagedetail(String date) {
         List<WageDetailInfo> wageDetailList = wageDetailInfoDao.findAll();
-        SimpleDateFormat sdfYear=new SimpleDateFormat("YYYY");
         DateTimeFormatter formatter1 = DateTimeFormatter.ofPattern("yyyy");
         DateTimeFormatter formatter2 = DateTimeFormatter.ofPattern("MM");
-        SimpleDateFormat sdfMonth=new SimpleDateFormat("MM");
         log.info("wagedetail--->{}",wageDetailList.size());
         if (wageDetailList!=null){
             int listSize = wageDetailList.size();
@@ -72,13 +77,11 @@ public class SynDataServiceImpl implements SynDataService {
                         log.info("wagedetail--->{}", dto);
                     }
                     //调用远程服务进行保存
-                    log.info("newDtoList--->",newDtoList.size());
-
+                    log.info("newDtoList--->{}",newDtoList.size());
+                    boolean b = synDataFeignController.syncWageDetail("2019", newDtoList);
                 }
             }
         }
-        List<WageDetailInfoDTO>  newDtoList = new ArrayList<>();
-        boolean b = synDataFeignController.syncWageDetail("2019", newDtoList);
         return null;
     }
 
@@ -88,8 +91,7 @@ public class SynDataServiceImpl implements SynDataService {
      */
     @Override
     public Integer empinfo() {
-        QEmployeeInfo employeeInfo=QEmployeeInfo.employeeInfo;
-        List<EmployeeInfo> employeeInfoListList = employeeInfoDao.select(employeeInfo).fetch();
+        List<EmployeeInfo> employeeInfoListList = employeeInfoDao.findAll();
         log.info("empinfo--->{}",employeeInfoListList.size());
         if (employeeInfoListList!=null){
             int listSize = employeeInfoListList.size();
@@ -130,28 +132,131 @@ public class SynDataServiceImpl implements SynDataService {
                 }
             }
         }
-
         return null;
     }
 
     @Override
     public Integer empwetchat() {
-        return null;
+        Integer result=0;
+        List<EmployeeWechatInfo> employeeInfoListList = employeeWechatInfoDao.findAll();
+        if (employeeInfoListList!=null){
+            int listSize = employeeInfoListList.size();
+            int toIndex = 100;
+            for (int i = 0; i<employeeInfoListList.size(); i+=100) {
+                if (i+100 > listSize){
+                    toIndex = listSize - i;
+                }
+                List<EmployeeWechatInfo> newList = employeeInfoListList.subList(i, i+toIndex);
+                List<EmployeeWechatInfoDTO> wechatInfoDTOS=null;
+                if (newList!=null){
+                    wechatInfoDTOS=new ArrayList<>();
+                    for (EmployeeWechatInfo wechat:newList){
+                        EmployeeWechatInfoDTO dto=new EmployeeWechatInfoDTO();
+                        BeanUtils.copyProperties(wechat,dto);
+                        wechatInfoDTOS.add(dto);
+                    }
+                    log.info("wechatInfoDTOS--->size:{}",wechatInfoDTOS.size());
+                    boolean b = synDataFeignController.syncEmpWetchat(wechatInfoDTOS);
+                    if (b){
+                        result+=wechatInfoDTOS.size();
+                    }
+                }
+            }
+        }
+        return result;
     }
 
     @Override
     public Integer enterprise() {
-        return null;
+        Integer result=0;
+        List<EntErpriseInfo> entErpriseInfoList = entErpriseInfoDao.findAll();
+        if (entErpriseInfoList!=null){
+            int listSize = entErpriseInfoList.size();
+            int toIndex = 100;
+            for (int i = 0; i<entErpriseInfoList.size(); i+=100) {
+                if (i+100 > listSize){
+                    toIndex = listSize - i;
+                }
+                List<EntErpriseInfo> newList = entErpriseInfoList.subList(i, i+toIndex);
+                List<EntErpriseInfoDTO> erpriseInfoDTOS=null;
+                if (newList!=null){
+                    erpriseInfoDTOS=new ArrayList<>();
+                    for (EntErpriseInfo wechat:newList){
+                        EntErpriseInfoDTO dto=new EntErpriseInfoDTO();
+                        BeanUtils.copyProperties(wechat,dto);
+                        erpriseInfoDTOS.add(dto);
+                    }
+                    log.info("erpriseInfoDTOS--->size:{}",erpriseInfoDTOS.size());
+                    boolean b = synDataFeignController.syncEnterprise(erpriseInfoDTOS);
+                    if (b){
+                        result+=erpriseInfoDTOS.size();
+                    }
+                }
+            }
+        }
+        return result;
     }
 
     @Override
     public Integer entgroup() {
-        return null;
+        Integer result=0;
+        List<EntGroupInfo> entGroupInfoList = entGroupInfoDao.findAll();
+        if (entGroupInfoList!=null){
+            int listSize = entGroupInfoList.size();
+            int toIndex = 100;
+            for (int i = 0; i<entGroupInfoList.size(); i+=100) {
+                if (i+100 > listSize){
+                    toIndex = listSize - i;
+                }
+                List<EntGroupInfo> newList = entGroupInfoList.subList(i, i+toIndex);
+                List<EntGroupInfoDTO> groupInfoDTOS=null;
+                if (newList!=null){
+                    groupInfoDTOS=new ArrayList<>();
+                    for (EntGroupInfo groupInfo:newList){
+                        EntGroupInfoDTO dto=new EntGroupInfoDTO();
+                        BeanUtils.copyProperties(groupInfo,dto);
+                        groupInfoDTOS.add(dto);
+                    }
+                    log.info("groupInfoDTOS--->size:{}",groupInfoDTOS.size());
+                    boolean b = synDataFeignController.syncEntGroup(groupInfoDTOS);
+                    if (b){
+                        result+=groupInfoDTOS.size();
+                    }
+                }
+            }
+        }
+        return result;
     }
 
     @Override
     public Integer manager() {
-        return null;
+        Integer result=0;
+        List<ManagerInfo> managerInfoList = managerInfoDao.findAll();
+        if (managerInfoList!=null){
+            int listSize = managerInfoList.size();
+            int toIndex = 100;
+            for (int i = 0; i<managerInfoList.size(); i+=100) {
+                if (i+100 > listSize){
+                    toIndex = listSize - i;
+                }
+                List<ManagerInfo> newList = managerInfoList.subList(i, i+toIndex);
+                List<ManagerInfoDTO> managerInfoDTOS=null;
+                if (newList!=null){
+                    managerInfoDTOS=new ArrayList<>();
+                    for (ManagerInfo managerInfo:newList){
+                        ManagerInfoDTO dto=new ManagerInfoDTO();
+                        BeanUtils.copyProperties(managerInfo,dto);
+                        managerInfoDTOS.add(dto);
+                    }
+                    log.info("managerInfoDTOS--->size:{}",managerInfoDTOS.size());
+                    boolean b = synDataFeignController.synManagerInfo(managerInfoDTOS);
+                    if (b){
+                        result+=managerInfoDTOS.size();
+                    }
+                }
+            }
+        }
+        return result;
     }
 
 
