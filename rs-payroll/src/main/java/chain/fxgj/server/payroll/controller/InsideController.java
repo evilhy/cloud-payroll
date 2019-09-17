150f70d7f8cf1d13bfa8e45407a011cc7bf1809f
kayak_zhucj <zhuchangjian@kayak.com.cn>
2019-09-17 21:58:50 +0800

    inside

--- rs-payroll/src/main/java/chain/fxgj/server/payroll/controller/InsideController.java
+++ rs-payroll/src/main/java/chain/fxgj/server/payroll/controller/InsideController.java
@@ -1,6 +1,7 @@
 package chain.fxgj.server.payroll.controller;
 
 import chain.css.exception.ParamsIllegalException;
+import chain.css.exception.ServiceHandleException;
 import chain.css.log.annotation.TrackLog;
 import chain.fxgj.core.common.constant.DictEnums.MsgBuisTypeEnum;
 import chain.fxgj.core.common.constant.ErrorConstant;
@@ -26,7 +27,6 @@ import org.apache.commons.lang3.StringUtils;
 import org.slf4j.MDC;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -56,6 +56,7 @@ public class InsideController {
     CallInsideService callInsideService;
     @Autowired
     InsideFeignService insideFeignService;
+
     /**
      * 发送短信验证码
      *
@@ -65,15 +66,19 @@ public class InsideController {
     @PostMapping("/sendCode")
     @TrackLog
     @PermitAll
-    public Res100302 sendCode(@RequestBody Req100302 req100302) {
+    public Mono<Res100302> sendCode(@RequestBody Req100302 req100302) {
+        Map<String, String> mdcContext = MDC.getCopyOfContextMap();
 
-        WageReq100302 wageIndexDTO = new WageReq100302();
-        wageIndexDTO.setPhone(req100302.getPhone());
+        return Mono.fromCallable(() -> {
+            MDC.setContextMap(mdcContext);
+            WageReq100302 wageIndexDTO = new WageReq100302();
+            wageIndexDTO.setPhone(req100302.getPhone());
 
-        WageRes100302 wageRes100302 = insideFeignService.sendCode(wageIndexDTO);
-        Res100302 res100302 = new Res100302();
-        BeanUtils.copyProperties(wageRes100302, res100302);
-        return res100302;
+            WageRes100302 wageRes100302 = insideFeignService.sendCode(wageIndexDTO);
+            Res100302 res100302 = new Res100302();
+            BeanUtils.copyProperties(wageRes100302, res100302);
+            return res100302;
+        }).subscribeOn(Schedulers.elastic());
     }
 
     /**
@@ -84,12 +89,15 @@ public class InsideController {
      */
     @TrackLog
     @PostMapping("/receipt")
-    public void receipt(@RequestBody ResReceiptDTO resReceiptDTO) {
-
-        WageResReceiptDTO wageResReceiptDTO = new WageResReceiptDTO();
-        BeanUtils.copyProperties(resReceiptDTO, wageResReceiptDTO);
-
-        insideFeignService.receipt(wageResReceiptDTO);
+    public Mono<Void> receipt(@RequestBody ResReceiptDTO resReceiptDTO) {
+        Map<String, String> mdcContext = MDC.getCopyOfContextMap();
+        return Mono.fromCallable(() -> {
+            MDC.setContextMap(mdcContext);
+            WageResReceiptDTO wageResReceiptDTO = new WageResReceiptDTO();
+            BeanUtils.copyProperties(resReceiptDTO, wageResReceiptDTO);
+            insideFeignService.receipt(wageResReceiptDTO);
+            return null;
+        }).subscribeOn(Schedulers.elastic()).then();
     }
 
     /**
@@ -113,7 +121,6 @@ public class InsideController {
             insideFeignService.readWage(wageReadWageDTO);
             return null;
         }).subscribeOn(Schedulers.elastic()).then();
-
     }
 
     /**
@@ -125,21 +132,25 @@ public class InsideController {
      */
     @PostMapping("/bindWX")
     @TrackLog
-    public void bandWX(@RequestBody Req100702 req100702) throws Exception {
-
+    public Mono<Void> bandWX(@RequestBody Req100702 req100702) throws Exception {
         Map<String, String> mdcContext = MDC.getCopyOfContextMap();
-        WageReq100702 wageReq100702 = new WageReq100702();
-        BeanUtils.copyProperties(req100702, wageReq100702);
-
         UserPrincipal userPrincipal = WebContext.getCurrentUser();
-        WageUserPrincipal wageUserPrincipal = new WageUserPrincipal();
-        BeanUtils.copyProperties(userPrincipal, wageUserPrincipal);
+        return Mono.fromCallable(() -> {
+            MDC.setContextMap(mdcContext);
 
-        WageBindRequestDTO wageBindRequestDTO = new WageBindRequestDTO();
-        wageBindRequestDTO.setWageReq100702(wageReq100702);
-        wageBindRequestDTO.setWageUserPrincipal(wageUserPrincipal);
+            WageReq100702 wageReq100702 = new WageReq100702();
+            BeanUtils.copyProperties(req100702, wageReq100702);
 
-        insideFeignService.bandWX(wageBindRequestDTO);
+            WageUserPrincipal wageUserPrincipal = new WageUserPrincipal();
+            BeanUtils.copyProperties(userPrincipal, wageUserPrincipal);
+
+            WageBindRequestDTO wageBindRequestDTO = new WageBindRequestDTO();
+            wageBindRequestDTO.setWageReq100702(wageReq100702);
+            wageBindRequestDTO.setWageUserPrincipal(wageUserPrincipal);
+
+            insideFeignService.bandWX(wageBindRequestDTO);
+            return null;
+        }).subscribeOn(Schedulers.elastic()).then();
     }
 
     /**
@@ -151,19 +162,28 @@ public class InsideController {
      */
     @PostMapping("/rz")
     @TrackLog
-    public void rz(@RequestBody Req100701 req100701) throws Exception {
-
-        WageReq100701 wageReq100701 = new WageReq100701();
-        BeanUtils.copyProperties(req100701, wageReq100701);
+    public Mono<Void> rz(@RequestBody Req100701 req100701) throws Exception {
+        Map<String, String> mdcContext = MDC.getCopyOfContextMap();
 
         UserPrincipal userPrincipal = WebContext.getCurrentUser();
-        WageUserPrincipal wageUserPrincipal = new WageUserPrincipal();
-        BeanUtils.copyProperties(userPrincipal, wageUserPrincipal);
+        return Mono.fromCallable(() -> {
+            MDC.setContextMap(mdcContext);
 
-        WageRzRequestDTO wageRzRequestDTO = new WageRzRequestDTO();
-        wageRzRequestDTO.setWageReq100701(wageReq100701);
-        wageRzRequestDTO.setWageUserPrincipal(wageUserPrincipal);
-        insideFeignService.rz(wageRzRequestDTO);
+            WageReq100701 wageReq100701 = new WageReq100701();
+            BeanUtils.copyProperties(req100701, wageReq100701);
+
+            WageUserPrincipal wageUserPrincipal = new WageUserPrincipal();
+            BeanUtils.copyProperties(userPrincipal, wageUserPrincipal);
+
+            WageRzRequestDTO wageRzRequestDTO = new WageRzRequestDTO();
+            wageRzRequestDTO.setWageReq100701(wageReq100701);
+            wageRzRequestDTO.setWageUserPrincipal(wageUserPrincipal);
+            String retStr = insideFeignService.rz(wageRzRequestDTO);
+            if (!StringUtils.equals("0000", retStr)) {
+                throw new ServiceHandleException(ErrorConstant.SYS_ERROR.format(retStr));
+            }
+            return null;
+        }).subscribeOn(Schedulers.elastic()).then();
     }
 
 
@@ -176,14 +196,19 @@ public class InsideController {
      */
     @PostMapping("/setPwd")
     @TrackLog
-    public void setPwd(@RequestBody String pwd) throws Exception {
+    public Mono<Void> setPwd(@RequestBody String pwd) throws Exception {
         Map<String, String> mdcContext = MDC.getCopyOfContextMap();
 
         UserPrincipal userPrincipal = WebContext.getCurrentUser();
-        WageUserPrincipal wageUserPrincipal = new WageUserPrincipal();
-        BeanUtils.copyProperties(userPrincipal, wageUserPrincipal);
+        return Mono.fromCallable(() -> {
+            MDC.setContextMap(mdcContext);
 
-        insideFeignService.setPwd(pwd, wageUserPrincipal);
+            WageUserPrincipal wageUserPrincipal = new WageUserPrincipal();
+            BeanUtils.copyProperties(userPrincipal, wageUserPrincipal);
+
+            insideFeignService.setPwd(pwd, wageUserPrincipal);
+            return null;
+        }).subscribeOn(Schedulers.elastic()).then();
     }
 
     /**
@@ -195,19 +220,26 @@ public class InsideController {
      */
     @PostMapping("/updPwd")
     @TrackLog
-    public void updPwd(@RequestBody UpdPwdDTO updPwdDTO) throws Exception {
-        WageUpdPwdDTO wageUpdPwdDTO = new WageUpdPwdDTO();
-        BeanUtils.copyProperties(updPwdDTO, wageUpdPwdDTO);
+    public Mono<Void> updPwd(@RequestBody UpdPwdDTO updPwdDTO) throws Exception {
+        Map<String, String> mdcContext = MDC.getCopyOfContextMap();
 
-        UserPrincipal currentUser = WebContext.getCurrentUser();
-        WageUserPrincipal wageUserPrincipal = new WageUserPrincipal();
-        BeanUtils.copyProperties(currentUser, wageUserPrincipal);
+        return Mono.fromCallable(() -> {
+            MDC.setContextMap(mdcContext);
 
-        WageUpPwdRequestDTO wageUpPwdRequestDTO = new WageUpPwdRequestDTO();
-        wageUpPwdRequestDTO.setQueryPwd(currentUser.getQueryPwd());
-        wageUpPwdRequestDTO.setWageUpdPwdDTO(wageUpdPwdDTO);
-        wageUpPwdRequestDTO.setWageUserPrincipal(wageUserPrincipal);
-        insideFeignService.updPwd(wageUpPwdRequestDTO);
+            WageUpdPwdDTO wageUpdPwdDTO = new WageUpdPwdDTO();
+            BeanUtils.copyProperties(updPwdDTO, wageUpdPwdDTO);
+
+            UserPrincipal currentUser = WebContext.getCurrentUser();
+            WageUserPrincipal wageUserPrincipal = new WageUserPrincipal();
+            BeanUtils.copyProperties(currentUser, wageUserPrincipal);
+
+            WageUpPwdRequestDTO wageUpPwdRequestDTO = new WageUpPwdRequestDTO();
+            wageUpPwdRequestDTO.setQueryPwd(currentUser.getQueryPwd());
+            wageUpPwdRequestDTO.setWageUpdPwdDTO(wageUpdPwdDTO);
+            wageUpPwdRequestDTO.setWageUserPrincipal(wageUserPrincipal);
+            insideFeignService.updPwd(wageUpPwdRequestDTO);
+            return null;
+        }).subscribeOn(Schedulers.elastic()).then();
     }
 
     /**
@@ -219,11 +251,21 @@ public class InsideController {
      */
     @PostMapping("/checkPhoneCode")
     @TrackLog
-    public void checkPhoneCode(@RequestBody ReqPhone reqPhone) throws Exception {
-        WageReqPhone wageReqPhone = new WageReqPhone();
-        BeanUtils.copyProperties(reqPhone, wageReqPhone);
+    public Mono<Void> checkPhoneCode(@RequestBody ReqPhone reqPhone) throws Exception {
+        Map<String, String> mdcContext = MDC.getCopyOfContextMap();
 
-        insideFeignService.checkPhoneCode(wageReqPhone);
+        return Mono.fromCallable(() -> {
+            MDC.setContextMap(mdcContext);
+
+            WageReqPhone wageReqPhone = new WageReqPhone();
+            BeanUtils.copyProperties(reqPhone, wageReqPhone);
+
+            String retStr = insideFeignService.checkPhoneCode(wageReqPhone);
+            if (!StringUtils.equals("0000", retStr)) {
+                throw new ServiceHandleException(ErrorConstant.SYS_ERROR.format(retStr));
+            }
+            return null;
+        }).subscribeOn(Schedulers.elastic()).then();
     }
 
     /**
@@ -235,20 +277,25 @@ public class InsideController {
      */
     @PostMapping("/updPhone")
     @TrackLog
-    public void updPhone(@RequestBody ReqPhone reqPhone) throws Exception {
-
-        WageReqPhone wageReqPhone = new WageReqPhone();
-        BeanUtils.copyProperties(reqPhone, wageReqPhone);
+    public Mono<Void> updPhone(@RequestBody ReqPhone reqPhone) throws Exception {
+        Map<String, String> mdcContext = MDC.getCopyOfContextMap();
 
         UserPrincipal userPrincipal = WebContext.getCurrentUser();
-        WageUserPrincipal wageUserPrincipal = new WageUserPrincipal();
-        BeanUtils.copyProperties(userPrincipal, wageReqPhone);
+        return Mono.fromCallable(() -> {
+            MDC.setContextMap(mdcContext);
+            WageReqPhone wageReqPhone = new WageReqPhone();
+            BeanUtils.copyProperties(reqPhone, wageReqPhone);
 
-        WageUpdPhoneRequestDTO wageUpdPhoneRequestDTO = new WageUpdPhoneRequestDTO();
-        wageUpdPhoneRequestDTO.setWageReqPhone(wageReqPhone);
-        wageUpdPhoneRequestDTO.setWageUserPrincipal(wageUserPrincipal);
+            WageUserPrincipal wageUserPrincipal = new WageUserPrincipal();
+            BeanUtils.copyProperties(userPrincipal, wageReqPhone);
 
-        insideFeignService.updPhone(wageUpdPhoneRequestDTO);
+            WageUpdPhoneRequestDTO wageUpdPhoneRequestDTO = new WageUpdPhoneRequestDTO();
+            wageUpdPhoneRequestDTO.setWageReqPhone(wageReqPhone);
+            wageUpdPhoneRequestDTO.setWageUserPrincipal(wageUserPrincipal);
+
+            insideFeignService.updPhone(wageUpdPhoneRequestDTO);
+            return null;
+        }).subscribeOn(Schedulers.elastic()).then();
     }
 
 
@@ -261,20 +308,26 @@ public class InsideController {
      */
     @PostMapping("/updBankCard")
     @TrackLog
-    public String updBankCard(@RequestBody UpdBankCardDTO updBankCardDTO) throws Exception {
-        WageUpdBankCardDTO wageUpdBankCardDTO = new WageUpdBankCardDTO();
-        BeanUtils.copyProperties(updBankCardDTO, wageUpdBankCardDTO);
+    public Mono<String> updBankCard(@RequestBody UpdBankCardDTO updBankCardDTO) throws Exception {
+        Map<String, String> mdcContext = MDC.getCopyOfContextMap();
 
         UserPrincipal userPrincipal = WebContext.getCurrentUser();
-        WageUserPrincipal wageUserPrincipal = new WageUserPrincipal();
-        BeanUtils.copyProperties(userPrincipal, wageUserPrincipal);
+        return Mono.fromCallable(() -> {
+            MDC.setContextMap(mdcContext);
 
-        WageUpdBankCardRequestDTO wageUpdBankCardRequestDTO = new WageUpdBankCardRequestDTO();
-        wageUpdBankCardRequestDTO.setWageUpdBankCardDTO(wageUpdBankCardDTO);
-        wageUpdBankCardRequestDTO.setWageUserPrincipal(wageUserPrincipal);
+            WageUpdBankCardDTO wageUpdBankCardDTO = new WageUpdBankCardDTO();
+            BeanUtils.copyProperties(updBankCardDTO, wageUpdBankCardDTO);
 
-        String retStr = insideFeignService.updBankCard(wageUpdBankCardRequestDTO);
-        return retStr;
+            WageUserPrincipal wageUserPrincipal = new WageUserPrincipal();
+            BeanUtils.copyProperties(userPrincipal, wageUserPrincipal);
+
+            WageUpdBankCardRequestDTO wageUpdBankCardRequestDTO = new WageUpdBankCardRequestDTO();
+            wageUpdBankCardRequestDTO.setWageUpdBankCardDTO(wageUpdBankCardDTO);
+            wageUpdBankCardRequestDTO.setWageUserPrincipal(wageUserPrincipal);
+
+            String retStr = insideFeignService.updBankCard(wageUpdBankCardRequestDTO);
+            return retStr;
+        }).subscribeOn(Schedulers.elastic());
     }
 
 }
