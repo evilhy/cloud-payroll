a39d609e8d85f924e02a67f44eceda111de24400
jinlin <305686443@qq.com>
2019-09-19 17:00:10 +0800

    解决feign异常抛出的问题

--- rs-payroll/src/main/java/chain/fxgj/core/common/config/FeignClientErrorDecoder.java
+++ rs-payroll/src/main/java/chain/fxgj/core/common/config/FeignClientErrorDecoder.java
@@ -1,9 +1,14 @@
 package chain.fxgj.core.common.config;
 
+import chain.css.exception.ErrorMsg;
+import chain.css.exception.ParamsIllegalException;
+import chain.fxgj.core.common.constant.ErrorConstant;
+import chain.utils.commons.JacksonUtil;
 import feign.Response;
 import feign.Util;
 import feign.codec.ErrorDecoder;
 import lombok.extern.slf4j.Slf4j;
+import net.sf.json.JSONObject;
 import org.springframework.context.annotation.Configuration;
 import reactor.core.Exceptions;
 import java.io.IOException;
@@ -24,7 +29,10 @@ public class FeignClientErrorDecoder implements ErrorDecoder {
         }
         log.info("body:{}",body);
         if (response.status() >= 400 && response.status() <= 500) {
-            throw Exceptions.failWithOverflow(body);
+            JSONObject object=JSONObject.fromObject(body);
+            String message=object.getString("message");
+            log.info("message:{}",body);
+            throw new ParamsIllegalException(new ErrorMsg("r008", message));
         }
         return errorStatus(methodKey, response);
     }
