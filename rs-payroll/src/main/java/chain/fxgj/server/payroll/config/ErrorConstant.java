312040d12ea929770aae1899a9980bb4cc3237d4
zhuchangjian <zhuchangjian@kayak.com.cn>
2020-03-30 16:32:51 +0800

    振兴银行-修改返回错误码code

--- rs-payroll/src/main/java/chain/fxgj/server/payroll/config/ErrorConstant.java
+++ rs-payroll/src/main/java/chain/fxgj/server/payroll/config/ErrorConstant.java
@@ -4,13 +4,13 @@ import chain.css.exception.ErrorMsg;
 
 public enum ErrorConstant {
 
-    MERCHANT_01("01", "非法访问!"),
-    MERCHANT_02("02", "验证签名失败!"),
-    MERCHANT_03("03", "解密失败!"),
-    MERCHANT_04("04", "加密失败!"),
-    MERCHANT_05("05", "SHA1安全加密失败!"),
-    MERCHANT_06("06", "请求超时!"),
-    MERCHANT_07("07", "信息未认证!"),;
+    MERCHANT_01("M001", "非法访问!"),
+    MERCHANT_02("M002", "验证签名失败!"),
+    MERCHANT_03("M003", "解密失败!"),
+    MERCHANT_04("M004", "加密失败!"),
+    MERCHANT_05("M005", "SHA1安全加密失败!"),
+    MERCHANT_06("M006", "请求超时!"),
+    MERCHANT_07("M007", "信息未认证!"),;
 
     private String errorCode;
     private String errorMsg;
