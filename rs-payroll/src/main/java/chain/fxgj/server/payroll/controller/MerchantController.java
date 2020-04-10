b39a67746dcb4a356595ccfe410239d896a51227
zhuchangjian <zhuchangjian@kayak.com.cn>
2020-04-10 18:13:58 +0800

    振兴银行-配合振兴测试version不取请求头的数据，用第三方公钥加密

--- rs-payroll/src/main/java/chain/fxgj/server/payroll/controller/MerchantController.java
+++ rs-payroll/src/main/java/chain/fxgj/server/payroll/controller/MerchantController.java
@@ -208,9 +208,12 @@ public class MerchantController {
                     .accessUrl(merchant.getAccessUrl())
                     .build();
             MerchantAccessDTO merchantAccess = MerchantAccessDTO.encryption(merchantAccessDTO, merchant.getParaRsaPublicKey());
+            log.info("merchantHeadDecrypt.getVersion():[{}]", merchantHeadDecrypt.getVersion());
+            merchantHeadDTO.setVersion(RSAEncrypt.encrypt(merchantHeadDecrypt.getVersion(), merchant.getParaRsaPublicKey()));
             log.info("accessUrl:[{}]", merchantAccess.getAccessUrl());
             log.info("version:[{}]", merchantHeadDTO.getVersion());
             log.info("appid:[{}]", merchantHeadDTO.getAppid());
+
             String retureSignature = MerchantAccessDTO.signature(merchantAccess, merchantHeadDTO);
             //String result = java.net.URLDecoder.decode(en ,"UTF-8");
 
