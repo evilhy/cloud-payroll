4ad836a6a17422697707a7927f3fbf5a51d99f25
kayak_zhucj <zhuchangjian@kayak.com.cn>
2020-08-19 22:21:27 +0800

    切库-核验接口调整-Constants

---
+++ rs-payroll/src/main/java/chain/fxgj/core/common/constant/Constants.java
@@ -0,0 +1,32 @@
+package chain.fxgj.core.common.constant;
+
+/**
+ * @author chain
+ * create by chain on 2018/9/12 下午6:30
+ **/
+public class Constants {
+    /**
+     * 默认日期时间格式
+     */
+    public static final String DEFAULT_DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss.SSS";
+    /**
+     * 默认日期格式
+     */
+    public static final String DEFAULT_DATE_FORMAT = "yyyy-MM-dd";
+    /**
+     * 默认时间格式
+     */
+    public static final String DEFAULT_TIME_FORMAT = "HH:mm:ss";
+
+    public static final Integer LOGIN_TIME_OUT = 30;
+    public static final Integer EMPUPLOAD_TIME_OUT = 5;
+
+    public static final Integer WELFARE_IMAGE_OUT = 1;
+
+    //红包雨活动默认时长 秒
+    public static final Integer RAIN_TIME = 60;
+    //红包雨活动默认时长 小时
+    public static final Integer RANDOM_TIME = 24;
+    //推送执行锁时长 小时
+    public static final Integer PUSH_TIME_OUT = 24;
+}
