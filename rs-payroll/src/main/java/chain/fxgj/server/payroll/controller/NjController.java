5c1402cdbfbc6c4ecb1e690c6e67a108fe681212
kayak_zhucj <zhuchangjian@kayak.com.cn>
2021-03-02 20:02:36 +0800

    南京银行-callback

--- rs-payroll/src/main/java/chain/fxgj/server/payroll/controller/NjController.java
+++ rs-payroll/src/main/java/chain/fxgj/server/payroll/controller/NjController.java
@@ -1,50 +1,41 @@
 package chain.fxgj.server.payroll.controller;
 
 
-import chain.css.exception.ParamsIllegalException;
 import chain.css.log.annotation.TrackLog;
-import chain.fxgj.core.common.constant.PayrollDBConstant;
-import chain.fxgj.server.payroll.config.ErrorConstant;
 import chain.fxgj.server.payroll.config.properties.MerchantsProperties;
-import chain.fxgj.server.payroll.constant.PayrollConstants;
-import chain.fxgj.server.payroll.dto.merchant.MerchantAccessDTO;
-import chain.fxgj.server.payroll.dto.merchant.MerchantDTO;
-import chain.fxgj.server.payroll.dto.merchant.MerchantHeadDTO;
 import chain.fxgj.server.payroll.dto.nj.NjRes100705;
 import chain.fxgj.server.payroll.service.EmployeeEncrytorService;
-import chain.fxgj.server.payroll.util.RSAEncrypt;
+import chain.fxgj.server.payroll.util.TransferUtil;
+import chain.fxgj.server.payroll.web.UserPrincipal;
+import chain.fxgj.server.payroll.web.WebContext;
 import chain.payroll.client.feign.EmployeeWechatFeignController;
 import chain.payroll.client.feign.MerchantFeignController;
 import chain.payroll.client.feign.NjFeignController;
 import chain.utils.commons.JacksonUtil;
-import chain.utils.commons.StringUtils;
-import chain.utils.commons.UUIDUtil;
 import chain.utils.fxgj.constant.DictEnums.AppPartnerEnum;
 import chain.utils.fxgj.constant.DictEnums.RegisterTypeEnum;
+import core.dto.response.NewestWageLogDTO;
 import core.dto.response.ent.EntIdGroupIdDTO;
-import core.dto.response.merchant.CacheEmpInfoDTO;
+import core.dto.response.ent.EntIdGroupIdReqDTO;
 import core.dto.response.merchant.CacheEmployeeWechatInfoDTO;
 import core.dto.response.merchant.CacheRes100705;
+import core.dto.wechat.CacheUserPrincipal;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.codec.binary.Base64;
 import org.slf4j.MDC;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 
 import javax.annotation.Resource;
-import java.text.SimpleDateFormat;
-import java.time.LocalDateTime;
-import java.time.ZoneOffset;
-import java.util.Date;
+import java.util.List;
 import java.util.Map;
-import java.util.Optional;
-import java.util.concurrent.TimeUnit;
 
 /**
  * 南京工资条  对外输出接口
@@ -68,176 +59,6 @@ public class NjController {
     EmployeeWechatFeignController employeeWechatFeignController;
     @Autowired
     NjFeignController njFeignController;
-    /**
-     * 根据appid 查询工资条接入合作方信息
-     *
-     * @param appid 合作方id
-     */
-    private MerchantsProperties.Merchant getMerchant(String appid) {
-
-        Optional<MerchantsProperties.Merchant> qWechat = merchantProperties.getMerchant().stream()
-                .filter(item -> item.getAppid().equalsIgnoreCase(appid)).findFirst();
-        MerchantsProperties.Merchant merchant = qWechat.orElseThrow(() -> {
-            log.error("==>appid={}，不存在，非法访问！", appid);
-            return new ParamsIllegalException(ErrorConstant.MERCHANT_01.getErrorMsg());
-        });
-        return merchant;
-    }
-
-    /**
-     * 访问凭证
-     *
-     * @param signature 签名
-     * @param appid     合作方id
-     * @param version   接口版本号
-     */
-    @PostMapping("/getAccess")
-    @TrackLog
-    public Mono<MerchantAccessDTO> getAccessUrl(@RequestHeader(value = "signature", required = true) String signature,
-                                                @RequestHeader(value = "appid", required = true) String appid,
-                                                @RequestHeader(value = "version", defaultValue = "1.0") String version,
-                                                @RequestHeader(value = "clientSn", required = true) String clientSn,
-                                                @RequestHeader(value = "clientDate", required = true) String clientDate,
-                                                @RequestHeader(value = "clientTime", required = true) String clientTime,
-                                                @RequestBody MerchantDTO merchantDTO,
-                                                ServerHttpResponse response
-    ) throws Exception {
-        Map<String, String> mdcContext = MDC.getCopyOfContextMap();
-        log.info("getAccess.signature:[{}]", signature);
-        log.info("getAccess.appid:[{}]", appid);
-        log.info("getAccess.version:[{}]", version);
-        log.info("getAccess.merchantDTO:[{}]", JacksonUtil.objectToJson(merchantDTO));
-        //【1】根据appid -->  取配置文件里商户信息
-        MerchantsProperties.Merchant merchant = this.getMerchant(StringUtils.trimToEmpty(appid));
-
-        //构建 head
-        MerchantHeadDTO merchantHeadDTO = MerchantHeadDTO.builder()
-                .version(version)
-                .signature(signature)
-                .appid(appid)
-                .build();
-        log.info("构建 MerchantHeadDTO:[{}]", JacksonUtil.objectToJson(merchantHeadDTO));
-        //解密
-        MerchantHeadDTO merchantHeadDecrypt = MerchantHeadDTO.decrypt(merchantHeadDTO, merchant.getRsaPrivateKey());
-        log.info("解密 MerchantHeadDTO:[{}]", JacksonUtil.objectToJson(merchantHeadDecrypt));
-
-        //1、解析 返回报文体信息
-        MerchantDTO merchantDecrypt = MerchantDTO.decrypt(merchantDTO, merchant.getRsaPrivateKey());
-
-        //2、生成签名信息
-        String checkSignature = MerchantDTO.signature(merchantDecrypt, merchantHeadDecrypt);
-
-        //3、对比签名信息
-        Base64 base64 = new Base64();
-        //signature base64 解密
-        signature = new String(base64.decode(signature), "UTF-8");
-        //signature 公钥 解密
-        signature = RSAEncrypt.decrypt(signature, merchant.getRsaPrivateKey());
-        log.info("签名信息，报文中={} ,解析后生成签名={} ", signature, checkSignature);
-
-        // todo 注释后测试使用暂不验签。生产请放开注释
-//        if (!signature.equalsIgnoreCase(checkSignature)) {
-//            log.error("签名信息,验证失败。报文中={} ,解析后生成签名={} ", signature, checkSignature);
-//            throw new ParamsIllegalException(ErrorConstant.MERCHANT_02.getErrorMsg());
-//        }
-        log.info(">>>>>>>验签通过<<<<<<<");
-
-        return Mono.fromCallable(() -> {
-            MDC.setContextMap(mdcContext);
-
-            //4、生成访问信息
-            String idNumber = employeeEncrytorService.encryptIdNumber(merchantDecrypt.getIdNumber().toUpperCase());
-            String phone = employeeEncrytorService.encryptPhone(merchantDecrypt.getPhone());
-            CacheEmployeeWechatInfoDTO paramWetchatDto = new CacheEmployeeWechatInfoDTO();
-
-            paramWetchatDto.setHeadimgurl(merchantDecrypt.getHeadimgurl());
-            paramWetchatDto.setUid(merchantDecrypt.getUid());
-            paramWetchatDto.setOpenId(merchantDecrypt.getOpenId());
-            paramWetchatDto.setNickname(merchantDecrypt.getNickname());
-            paramWetchatDto.setIdNumber(idNumber);
-            paramWetchatDto.setPhone(phone);
-            paramWetchatDto.setAppPartner(merchant.getMerchantCode().getCode());
-            paramWetchatDto.setRegisterType(RegisterTypeEnum.UUID.getCode());
-            CacheEmployeeWechatInfoDTO wechatInfoDTO = employeeWechatFeignController.findempwechat(paramWetchatDto);
-            if (wechatInfoDTO != null) {  //认证绑定信息表已经存在，则说明已经绑定成功
-                log.info("认证绑定信息表【存在】，则说明已经绑定成功！");
-                wechatInfoDTO.setNickname(paramWetchatDto.getNickname());
-                wechatInfoDTO.setHeadimgurl(paramWetchatDto.getHeadimgurl());
-                wechatInfoDTO.setUid(paramWetchatDto.getUid());
-                wechatInfoDTO.setOpenId(paramWetchatDto.getOpenId());
-                wechatInfoDTO.setRegisterType(RegisterTypeEnum.UUID.getCode());
-                wechatInfoDTO.setAppPartner(merchant.getMerchantCode().getCode());
-                log.info("wechatInfoDTO入库:[{}]", JacksonUtil.objectToJson(wechatInfoDTO));
-                employeeWechatFeignController.saveempwechat(wechatInfoDTO);
-            } else {
-                log.info("认证绑定信息表【不存在】！");
-                //查询用户信息
-                CacheEmpInfoDTO wageEmpInfoDTO = new CacheEmpInfoDTO();
-                wageEmpInfoDTO.setIdNumber(merchantDecrypt.getIdNumber());
-                wageEmpInfoDTO.setName(merchantDecrypt.getName());
-                CacheEmpInfoDTO empInfoDTO = employeeWechatFeignController.findemp(wageEmpInfoDTO);
-                if (empInfoDTO != null) {
-                    log.info("员工信息表中【存在】！入库:[{}]", JacksonUtil.objectToJson(paramWetchatDto));
-                    wechatInfoDTO = employeeWechatFeignController.saveempwechat(paramWetchatDto);
-                } else {
-                    log.info("员工信息表中【不存在】！信息未认证");
-                    throw new ParamsIllegalException(ErrorConstant.MERCHANT_07.getErrorMsg());
-                }
-
-            }
-
-
-            String accessToken = UUIDUtil.createUUID8();
-            log.info("accessToken:[{}]", accessToken);
-            String redisKey = PayrollDBConstant.PREFIX + ":merchant:" + accessToken;
-            merchantDecrypt.setDataAuths(merchant.getDataAuths());
-            merchantDecrypt.setAppPartner(merchant.getMerchantCode());
-            merchantDecrypt.setRegisterType(RegisterTypeEnum.UUID);
-            String emp = JacksonUtil.objectToJson(merchantDecrypt);
-            redisTemplate.opsForValue().set(redisKey, emp, PayrollConstants.MERCHANT_EXPIRESIN, TimeUnit.SECONDS);
-
-            //5、生成签名信息
-
-            MerchantAccessDTO merchantAccessDTO = MerchantAccessDTO.builder()
-                    .accessToken(accessToken)
-                    .expiresIn(PayrollConstants.MERCHANT_EXPIRESIN)
-                    .accessUrl(merchant.getAccessUrl())
-                    .build();
-            MerchantAccessDTO merchantAccess = MerchantAccessDTO.encryption(merchantAccessDTO, merchant.getParaRsaPublicKey());
-
-            String encryptVersion = RSAEncrypt.encrypt("1.0", merchant.getParaRsaPublicKey());
-            log.info("encryptAccessUrl:[{}]", merchantAccess.getAccessUrl());
-            log.info("encryptVersion:[{}]", encryptVersion);
-            log.info("appid:[{}]", merchant.getAppid());
-            String retureSignature = MerchantAccessDTO.signatureSHA(merchantAccess.getAccessUrl(), encryptVersion, merchant.getAppid());
-            log.info("accessUrl.version.appid签名：{}", retureSignature);
-
-            //公钥加密
-            retureSignature = RSAEncrypt.encrypt(retureSignature, merchant.getParaRsaPublicKey());
-            log.info("==>retureSignature签名使用公钥再加密 ={}", retureSignature);
-
-            //signature base64
-            retureSignature = base64.encodeToString(retureSignature.getBytes("UTF-8"));
-            log.info("==>retureSignature base64 ={}", retureSignature);
-
-            response.getHeaders().set("signature", retureSignature);
-            log.info("headers.signature返回签名：{}", retureSignature);
-            LocalDateTime now = LocalDateTime.now();
-            String encryptLog = RSAEncrypt.encrypt("1.0", merchant.getParaRsaPublicKey());
-            log.info("encryptLog:[{}]", encryptLog);
-            response.getHeaders().set("version", encryptVersion);
-            response.getHeaders().set("clientSn", UUIDUtil.createUUID32());
-
-            response.getHeaders().set("clientDate", new SimpleDateFormat("yyyyMMdd").format(new Date()));
-
-            long timeStamp = now.toInstant(ZoneOffset.of("+8")).toEpochMilli();
-            response.getHeaders().set("clientTime", String.valueOf(timeStamp));
-            log.info("merchantAccess:[{}]", JacksonUtil.objectToJson(merchantAccess));
-            return merchantAccess;
-        }).subscribeOn(Schedulers.elastic());
-
-    }
-
 
     /**
      * 访问凭证
@@ -259,21 +80,19 @@ public class NjController {
             cacheEmployeeWechatInfoDTOReq.setAppPartner(AppPartnerEnum.NJCB.getCode());
             cacheEmployeeWechatInfoDTOReq.setRegisterType(RegisterTypeEnum.UUID.getCode());
             CacheEmployeeWechatInfoDTO wechatInfoDTO = employeeWechatFeignController.findEmpwechatInfo(cacheEmployeeWechatInfoDTOReq);
+            log.info("nj.wechatInfoDTO:[{}]", JacksonUtil.objectToJson(wechatInfoDTO));
 
             if (wechatInfoDTO != null) {
                 CacheRes100705 wageRes100705 = njFeignController.wxCallback(accessToken);
                 if (res100705 != null) {
                     BeanUtils.copyProperties(wageRes100705, res100705);
                     res100705.setApppartner(chain.utils.fxgj.constant.DictEnums.AppPartnerEnum.values()[wageRes100705.getApppartner()]);
-                    String entCustNo = wechatInfoDTO.getEntCustNo();
-                    //根据企业直联客户号，查entId，groupId(一个企业，对应一个机构)
-                    EntIdGroupIdDTO entIdGroupIdDTO = njFeignController.findByEntCustNo(entCustNo);
-                    if (null != entIdGroupIdDTO) {
-                        res100705.setEntId(entIdGroupIdDTO.getEntId());
-                        res100705.setGroupId(entIdGroupIdDTO.getGroupId());
-                    } else {
-                        log.error("据企业直联客户号未查entId，groupId");
-                    }
+                    EntIdGroupIdReqDTO entIdGroupIdReqDTO = EntIdGroupIdReqDTO.builder()
+                            .entCustNo(wechatInfoDTO.getEntCustNo())
+                            .encryptIdNumber(wechatInfoDTO.getIdNumber())
+                            .build();
+                    List<EntIdGroupIdDTO> entIdGroupIdDTOList = njFeignController.findByEntCustNo(entIdGroupIdReqDTO);
+                    res100705.setEntIdGroupIdDTOList(entIdGroupIdDTOList);
                 }
             } else {
                 log.error("用户信息不存在！");
--- rs-payroll/src/main/java/chain/fxgj/server/payroll/dto/nj/NjRes100705.java
+++ rs-payroll/src/main/java/chain/fxgj/server/payroll/dto/nj/NjRes100705.java
@@ -2,7 +2,10 @@ package chain.fxgj.server.payroll.dto.nj;
 
 import chain.fxgj.server.payroll.constant.DictEnums.IsStatusEnum;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import core.dto.response.ent.EntIdGroupIdDTO;
 import lombok.*;
+
+import java.util.List;
 
 /**
  * Res100705
@@ -76,10 +79,11 @@ public class NjRes100705 {
      */
     private String groupId;
 
+    /**
+     * 企业列表(多企业-单机构)
+     */
+    private List<EntIdGroupIdDTO> entIdGroupIdDTOList;
 
-    public NjRes100705(String jsessionId, String bindStatus) {
-        this.jsessionId = jsessionId;
-        this.bindStatus = bindStatus;
-    }
+
 
 }
