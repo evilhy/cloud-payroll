25113cc6918a183cfe243460b9f93a98dd50463f
du <zaipengdu@163.com>
2021-09-10 15:10:29 +0800

    用户签约地址

--- rs-payroll/src/main/java/chain/fxgj/server/payroll/controller/TaxController.java
+++ rs-payroll/src/main/java/chain/fxgj/server/payroll/controller/TaxController.java
@@ -264,21 +264,33 @@ public class TaxController {
                 System.out.println("finish");
             }).subscribe();
 
+
+
             //图片压缩
             File file = new File(filePath);
+            String compressionPath = filePath;
             if (file.length() > 1024 * 120) {
-                ImgPicUtils.compression(filePath, filePath);
+                //创建图片压缩目录
+                String path = file.getPath();
+                String name = file.getName();
+                String replace = path.replace(name, "min/");
+                File file1 = new File(replace);
+                if (!file1.exists()){
+                    file1.mkdirs();
+                }
+                compressionPath = replace + name;
+                ImgPicUtils.compression(filePath, compressionPath);
             }
 
             //身份证照片
-            String base64 = ImageBase64Utils.imageToBase64(filePath);
+            String base64 = ImageBase64Utils.imageToBase64(compressionPath);
             if (StringUtils.isBlank(base64)) {
-                log.info("=====> 图片上传失败，请重新上传。filePath：{}", filePath);
+                log.info("=====> 图片上传失败，请重新上传。compressionPath：{}", compressionPath);
                 throw new ParamsIllegalException(ErrorConstant.SYS_ERROR.format("图片上传失败，请重新上传"));
             }
 
             return UploadDto.builder()
-                    .filepath(filePath)
+                    .filepath(compressionPath)
                     .imgBase("data:image/jpg;base64," + base64)
                     .build();
         }).subscribeOn(Schedulers.elastic());
